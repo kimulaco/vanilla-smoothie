ac36f56d216e5f17722c0170d6d9ba2e94534dde
kimulaco <kmrkdev@gmail.com>
2019-04-20 21:05:36 +0900

    fix package version

--- docs/js/vanilla-smoothie.js
+++ docs/js/vanilla-smoothie.js
@@ -1,5 +1,5 @@
 /** 
- * VanillaSmoothie.js v1.2.0
+ * VanillaSmoothie.js v1.2.1
  * https://kimulaco.github.io/vanilla-smoothie/
  * Copyright (c) 2019 kimulaco
  * This software is released under the MIT License.
